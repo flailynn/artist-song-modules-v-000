0c33b2ef61619636edb6a912f030b55667544642
Anita Beauchamp <amadamspr@gmail.com>
2018-08-01 04:25:45 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,8 @@
 require 'pry'
 # require_relative '../lib/concerns/memorable'
 class Artist
-  extend Memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   extend Findable
   include Paramable
   attr_accessor :name
